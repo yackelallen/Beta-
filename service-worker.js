3cf8a0876b7c9df558609cb4ec3195efd5db527c
yackelallen <yackelcole@gmail.com>
2026-06-01 21:18:06 -0500

    Refactor service worker for caching strategy
    Updated cache name and modified service worker events to cache specified URLs.

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,24 @@
-const CACHE_NAME = "yit-beta-cache-v1";
+const CACHE_NAME = "yit-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-512.PNG"
+];
 
 self.addEventListener("install", event => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key.startsWith("yit-beta-cache-") && key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener("message", event => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
 self.addEventListener("fetch", event => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      fetch(event.request, { cache: "no-store" })
-        .catch(() => caches.match("./index.html"))
-    );
-  }
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
